a25100f07b28eba6072736aeafea796b6f0d7983
m1taylor <m1taylor@ryerson.ca>
2023-05-12 02:15:24 -0400

    figured out how to link tailwind and next with ts and postcss

---
+++ components/Logo.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Logonew from '../public/images/axislogo4.png'
+
+
+export default function Logo() {
+  return (
+    <Link href="/" className="block" aria-label="Cruip">
+<div className="">
+        <Image src={Logonew} className="w-36 lg:w-56" />
+      </div>
+    </Link>
+  )
+}
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import "../styles/globals.css";
+import "../css/style.css";
+
 import type { AppProps } from "next/app";
-
+import Layout from "components/Layout";
+import Footer from "components/Footer";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <html>
+      <head>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
 
+
+
+      </head>
+      <body>
+    <Layout>
+      <Component {...pageProps} />;
+      <Footer />
+    </Layout>
+    </body>
+      </html>
+  );
+}
 export default MyApp;
