0c20fff8d4889a4c891cf7ce642c56263cf0631a
m1taylor <m1taylor@ryerson.ca>
2023-05-24 20:44:19 -0400

    troubleshooting more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,9 @@ import "../css/style.css";
 import type { AppProps } from "next/app";
 import Layout from "components/Layout";
 import Footer from "components/Footer";
-import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
       <Layout>
         <Component {...pageProps} />
         <Footer />
