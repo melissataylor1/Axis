b4c0c79a81e4a3baa492f41e2674e0ac15f6a15e
m1taylor <m1taylor@ryerson.ca>
2023-05-24 20:40:58 -0400

    troubleshooting favicon:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,18 @@ import "../css/style.css";
 import type { AppProps } from "next/app";
 import Layout from "components/Layout";
 import Footer from "components/Footer";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Footer />
-    </Layout>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+        <Footer />
+      </Layout>
+    </>
   );
 }
 export default MyApp;
