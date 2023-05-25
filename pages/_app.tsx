234dd887682d30ab0eb488128d7387afe2f8b75e
m1taylor <m1taylor@ryerson.ca>
2023-05-24 20:38:15 -0400

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,13 @@ import "../css/style.css";
 import type { AppProps } from "next/app";
 import Layout from "components/Layout";
 import Footer from "components/Footer";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-        <Footer />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+      <Footer />
+    </Layout>
   );
 }
-
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { NextPage } from "next";
-
 import React from "react";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
