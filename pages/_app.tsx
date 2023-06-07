e23f3faeab267f4711044ec6fdf0f2db7c135027
m1taylor <m1taylor@ryerson.ca>
2023-06-07 16:18:19 -0400

    added meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../css/style.css";
 import type { AppProps } from "next/app";
+
 import Layout from "components/Layout";
 import Footer from "components/Footer";
 function MyApp({ Component, pageProps }: AppProps) {
